712739a5fa814bb54107adeaa445a367384325f6
Murilo Ferreira <muriloout@outlook.com>
2026-04-02 22:46:23 -0300

    second commit

---
+++ src/main/java/com/muriloDev/osloIA/OsloIaApplication.java
@@ -0,0 +1,13 @@
+package com.muriloDev.osloIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsloIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsloIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muriloDev/osloIA/OsloIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloDev.osloIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsloIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
